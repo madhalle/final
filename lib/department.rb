a3bba19e2bed1de137cfaef51313b821c2658597
Mad Hallé <mad.halle@icloud.com>
2020-04-15 20:29:57 -0600

    iteration 2 complete

--- lib/department.rb
+++ lib/department.rb
@@ -1,11 +1,16 @@
 class Department
-  attr_reader :name, :employees
+  attr_reader :name, :employees, :expenses
   def initialize(name)
     @name = name
     @employees = []
+    @expenses = 0
   end
 
   def hire(employee)
     @employees << employee
+  end
+
+  def expense(cost)
+    @expenses += cost
   end
 end
